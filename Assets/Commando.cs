f241808e3c8b34fbd5ef13e3b3a9397a85c16a3b
Benjamin Overgaard <benjamin.n.overgaard@gmail.com>
2017-04-22 16:39:42 +0200

    Player can be killed. Need game reset.

---
+++ Assets/Commando.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Commando : DestroyableObject {
+
+	private Vector3 _startPos;
+
+	protected override void Start()
+	{
+		base.Start();
+
+		_startPos = transform.position;
+	}
+
+	public override void ReviveObject()
+	{
+		base.ReviveObject();
+
+		transform.position = _startPos;
+	}
+}
