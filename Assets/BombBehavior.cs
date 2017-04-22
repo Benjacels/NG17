4156547866ee2d48d31190d83de57e46e5082ab0
Benjamin Overgaard <benjamin.n.overgaard@gmail.com>
2017-04-22 16:16:28 +0200

    Bomb delay

--- Assets/BombBehavior.cs
+++ Assets/BombBehavior.cs
@@ -7,6 +7,7 @@ public class BombBehavior : MonoBehaviour {
 	public float ScaleSpeed = 20;
 	public float BombRadius = 3;
 	public float DestroyDelay = 0.1f;
+	public float BombSendDelay = 1;
 
 	private SphereCollider _collider;
 	private SpriteRenderer _spriteRenderer;
@@ -18,19 +19,33 @@ public class BombBehavior : MonoBehaviour {
 
 	void Awake()
 	{
-		_collider = GetComponent<SphereCollider>();
-		_spriteRenderer = GetComponent<SpriteRenderer>();
-		_collider.enabled = false;
-		_collider.radius *= BombRadius;
-
-		_startSize = transform.localScale;
-		_targetSize = transform.localScale / TargetSize;
+		
 	}
 
 	// Use this for initialization
 	void Start () {
-		_dropBomb = true;
+
+		_collider = GetComponent<SphereCollider>();
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+
+		_spriteRenderer.enabled = false;
+		_collider.enabled = false;
+
+		_collider.radius *= BombRadius;
+
+		_startSize = transform.localScale;
+		_targetSize = transform.localScale / TargetSize;
+
+		StartCoroutine("DelayBomb");
+	}
+
+	IEnumerator DelayBomb()
+	{
+		yield return new WaitForSeconds(BombSendDelay);
+
 		_startTime = Time.time;
+		_spriteRenderer.enabled = true;
+		_dropBomb = true;
 	}
 	
 	// Update is called once per frame
@@ -41,7 +56,12 @@ public class BombBehavior : MonoBehaviour {
 	void DropBomb()
 	{
 		if (!_dropBomb)
+		{
+			_spriteRenderer.enabled = false;
+			_collider.enabled = false;
+
 			return;
+		}
 
 		float dist = Vector3.Distance(_startSize, _targetSize);
 		float progressTime = (Time.time - _startTime) * ScaleSpeed;
