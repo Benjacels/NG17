d3c97e56a7f713ddcd5dcae79cc58c33a0a0eea6
Fjellvang <drk441@alumni.ku.dk>
2017-04-22 21:18:57 +0200

    add soldier sprites

--- Assets/Commando.cs
+++ Assets/Commando.cs
@@ -5,11 +5,15 @@ public class Commando : DestroyableObject {
 
 	private Vector3 _startPos;
 
+    // gem originale transform værdier, så vi kan ændre orientering, uden at fucke inspector værdier op
+    private Transform _origPos;
+
 	protected override void Start()
 	{
 		base.Start();
 
 		_startPos = transform.position;
+        _origPos = transform;
 	}
 
 	public override void ReviveObject()
@@ -30,4 +34,32 @@ public class Commando : DestroyableObject {
 	{
 		GameObject.FindWithTag("ResetGameParent").transform.GetChild(0).gameObject.SetActive(true);
 	}
+
+    public void FaceUp()
+    {
+        // set back to orig transform, not position obviously
+        transform.localScale = _origPos.localScale;
+        transform.rotation = _origPos.rotation;
+    }
+    
+    public void FaceDown()
+    {
+        // negate scale y coordinate
+        transform.localScale = new Vector3(_origPos.localScale.x, -_origPos.localScale.y, _origPos.localScale.z);
+        transform.rotation = _origPos.rotation;
+    }
+
+    public void FaceRight()
+    {
+        //rotate 90 degrees around z, negate scale y
+        transform.localScale = new Vector3(_origPos.localScale.x, -_origPos.localScale.y, _origPos.localScale.z);
+        transform.rotation.SetEulerAngles(0, 0, 90); // Ugh, deprecated, håber det duer
+    }
+
+    public void FaceLeft()
+    {
+        //Rotate 90 degrees, orig scale
+        transform.localScale = _origPos.localScale;
+        transform.rotation.SetEulerAngles(0, 0, 90);
+    }
 }
