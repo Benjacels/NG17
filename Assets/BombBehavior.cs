3271e47ab4df79e277796d04ad192fd7d147a1e3
Benjamin Overgaard <benjamin.n.overgaard@gmail.com>
2017-04-22 15:31:54 +0200

    Bomb works

---
+++ Assets/BombBehavior.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombBehavior : MonoBehaviour {
+
+	public float TargetSize = 10;
+	public float ScaleSpeed = 20;
+	public float BombRadius = 3;
+	public float DestroyDelay = 0.1f;
+
+	private SphereCollider _collider;
+	private SpriteRenderer _spriteRenderer;
+	private bool _dropBomb = false;
+	private float _startTime;
+
+	private Vector3 _startSize;
+	private Vector3 _targetSize;
+
+	void Awake()
+	{
+		_collider = GetComponent<SphereCollider>();
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+		_collider.enabled = false;
+
+		_startSize = transform.localScale;
+		_targetSize = transform.localScale / TargetSize;
+	}
+
+	// Use this for initialization
+	void Start () {
+		_dropBomb = true;
+		_startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DropBomb();
+	}
+
+	void DropBomb()
+	{
+		if (!_dropBomb)
+			return;
+
+		float dist = Vector3.Distance(_startSize, _targetSize);
+		float progressTime = (Time.time - _startTime) * ScaleSpeed;
+
+		float progression = progressTime / dist;
+
+		transform.localScale = Vector3.Lerp(_startSize, _targetSize, progression);
+
+		if (transform.localScale == _targetSize)
+			Explode();
+	}
+
+	void Explode()
+	{
+		_collider.enabled = true;
+		_collider.radius *= BombRadius;
+
+		_spriteRenderer.enabled = false;
+
+		DestroyObject(gameObject, DestroyDelay);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<DestroyableObject>() != null)
+			other.GetComponent<DestroyableObject>().DestroyObject();
+	}
+}
---
+++ Assets/DangerClose/Scripts/DestroyableObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyableObject : MonoBehaviour {
+
+	public enum DestroyableObjectState
+	{
+		Alive,
+		Destroyed
+	}
+
+	private SpriteRenderer _spriteRenderer;
+
+	public Sprite AliveSprite;
+	public Sprite DestroyedSprite;
+
+	private DestroyableObjectState _currentDestroyState;
+
+	void Start()
+	{
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+		ReviveObject();
+	}
+
+	public virtual void DestroyObject()
+	{
+		_spriteRenderer.sprite = DestroyedSprite;
+		_currentDestroyState = DestroyableObjectState.Destroyed;
+	}
+
+	public virtual void ReviveObject()
+	{
+		_spriteRenderer.sprite = AliveSprite;
+		_currentDestroyState = DestroyableObjectState.Alive;
+	}
+}
